d2ff89e04a73a9a997d80b759e081d9183bb1e6b
kenresoft <amadikenneth8@gmail.com>
2024-03-19 21:44:13 +0100

    Add onboarding.dart

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:smart_pay_mobile/screens/sign_in.dart';
 
 class Onboarding extends StatefulWidget {
   const Onboarding({super.key});
@@ -35,9 +35,9 @@ class _OnboardingState extends State<Onboarding> {
                 ),
               ),
             ),
-            const SizedBox(height: 83),
+            const SizedBox(height: 123),
             Container(
-              width: 253,
+              width: double.infinity,
               height: 359,
               decoration: const BoxDecoration(
                 image: DecorationImage(
@@ -45,123 +45,9 @@ class _OnboardingState extends State<Onboarding> {
                   fit: BoxFit.fill,
                 ),
               ),
-              child: Stack(
-                children: [
-                  Positioned(
-                    top: 38.44,
-                    left: 11.42,
-                    child: Transform(
-                      transform: Matrix4.identity()
-                        ..translate(0.0, 0.0)
-                        ..rotateZ(-0.11),
-                      child: Container(
-                        width: 48.15,
-                        height: 48.15,
-                        decoration: ShapeDecoration(
-                          color: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(1000),
-                          ),
-                          shadows: const [
-                            BoxShadow(
-                              color: Color(0x289BA3AF),
-                              blurRadius: 25,
-                              offset: Offset(-5, 10),
-                              spreadRadius: 0,
-                            )
-                          ],
-                        ),
-                        child: SvgPicture.asset('assets/icons/fluent_shield.svg'),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: 152,
-                    left: 130,
-                    child: Container(
-                      width: 160,
-                      height: 97.67,
-                      decoration: ShapeDecoration(
-                        color: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(11.16),
-                        ),
-                        shadows: const [
-                          BoxShadow(
-                            color: Color(0x0F6B727F),
-                            blurRadius: 25,
-                            offset: Offset(-15, 15),
-                            spreadRadius: 0,
-                          )
-                        ],
-                      ),
-                      child: const Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(top: 11.16, left: 12.09),
-                            child: SizedBox(
-                              width: 50.23,
-                              height: 13.95,
-                              child: Text(
-                                'This month',
-                                style: TextStyle(
-                                  color: Color(0xFF111827),
-                                  fontSize: 9.30,
-                                  fontFamily: 'SF Pro Display',
-                                  fontWeight: FontWeight.w600,
-                                  height: 0.16,
-                                  letterSpacing: 0.28,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: 229.2,
-                    child: Container(
-                      width: 142,
-                      height: 51,
-                      decoration: ShapeDecoration(
-                        color: Colors.white,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-                        shadows: const [
-                          BoxShadow(
-                            color: Color(0x149BA3AF),
-                            blurRadius: 25,
-                            offset: Offset(-5, 10),
-                            spreadRadius: 0,
-                          )
-                        ],
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.only(top: 11, left: 11),
-                        child: SizedBox(
-                          width: 50.23,
-                          height: 13.95,
-                          child: Text(
-                            'This month',
-                            style: TextStyle(
-                              color: Color(0xFF111827),
-                              fontSize: 9.30,
-                              fontFamily: 'SF Pro Display',
-                              fontWeight: FontWeight.w600,
-                              height: 0.16,
-                              letterSpacing: 0.28,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
             ),
             const Padding(
-              padding: EdgeInsets.only(top: 10),
+              padding: EdgeInsets.only(top: 80),
               child: SizedBox(
                 width: 287,
                 child: Text(
@@ -204,35 +90,43 @@ class _OnboardingState extends State<Onboarding> {
                 ),
               ),
             ),
-            SizedBox(height: 34),
-            Container(
-              width: 287,
-              height: 56,
-              padding: const EdgeInsets.all(8),
-              decoration: ShapeDecoration(
-                color: Color(0xFF111827),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
+            const SizedBox(height: 34),
+            GestureDetector(
+              onTap: () => Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SignIn(),
                 ),
               ),
-              child: const Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'Get Started',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontFamily: 'SF Pro Display',
-                      fontWeight: FontWeight.w700,
-                      height: 0.09,
-                      letterSpacing: 0.30,
-                    ),
+              child: Container(
+                width: 287,
+                height: 56,
+                padding: const EdgeInsets.all(8),
+                decoration: ShapeDecoration(
+                  color: const Color(0xFF111827),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
                   ),
-                ],
+                ),
+                child: const Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Get Started',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontFamily: 'SF Pro Display',
+                        fontWeight: FontWeight.w700,
+                        height: 0.09,
+                        letterSpacing: 0.30,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             )
           ],
