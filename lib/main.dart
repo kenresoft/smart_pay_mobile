824bec140de71e1034386cd28f851c59ee2af45f
kenresoft <amadikenneth8@gmail.com>
2024-03-22 17:39:18 +0100

    Update project

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
+import 'package:extensionresoft/extensionresoft.dart';
 import 'package:flutter/material.dart';
+import 'package:smart_pay_mobile/screens/confirmation.dart';
+import 'package:smart_pay_mobile/screens/identity.dart';
 import 'package:smart_pay_mobile/screens/onboarding.dart';
+import 'package:smart_pay_mobile/screens/otp.dart';
+import 'package:smart_pay_mobile/screens/password_recovery.dart';
+import 'package:smart_pay_mobile/screens/pin.dart';
+import 'package:smart_pay_mobile/screens/reset_password.dart';
 import 'package:smart_pay_mobile/screens/sign_in.dart';
 import 'package:smart_pay_mobile/screens/sign_up.dart';
 import 'package:smart_pay_mobile/screens/splash.dart';
+import 'package:smart_pay_mobile/screens/verify_identity.dart';
+import 'package:smart_pay_mobile/services/prefs.dart';
+import 'package:smart_pay_mobile/utils/constants.dart';
+import 'package:smart_pay_mobile/utils/routes.dart';
+import 'package:smart_pay_mobile/utils/utils.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedPreferencesService.init();
+  final deviceName = await Utils.getDeviceName();
+  device = deviceName?? Constants.unknownDevice;
+  print(deviceName);
   runApp(const MyApp());
 }
 
@@ -26,11 +43,18 @@ class MyApp extends StatelessWidget {
         splashColor: Colors.transparent,
       ),
       routes: {
-        //'/': (context) => const Home(),
-        '/': (context) => const Splash(),
-        '/onboarding': (context) => const Onboarding(),
-        '/sign_up': (context) => const SignUp(),
-        '/sign_in': (context) => const SignIn(),
+        //RouteConstants.splash: (context) => const Splash(),
+        RouteConstants.splash: (context) => const Splash(),
+        RouteConstants.onboarding: (context) => const Onboarding(),
+        RouteConstants.signUp: (context) => const SignUp(),
+        RouteConstants.signIn: (context) => const SignIn(),
+        RouteConstants.otp: (context) => const OtpScreen(),
+        RouteConstants.identity: (context) => const Identity(),
+        RouteConstants.pin: (context) => const PinScreen(),
+        RouteConstants.confirmation: (context) => const Confirmation(),
+        RouteConstants.passwordRecovery: (context) => const PasswordRecovery(),
+        RouteConstants.verifyIdentity: (context) => const VerifyIdentity(),
+        RouteConstants.resetPassword: (context) => const ResetPassword(),
       },
     );
   }
