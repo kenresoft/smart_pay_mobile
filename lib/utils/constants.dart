efd8c944d2e4db830640f370f7dc1372141e4691
kenresoft <amadikenneth8@gmail.com>
2024-03-22 20:35:19 +0100

    Update constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  Constants._();
+
+  static const empty = '';
+  static const unknownDevice = 'Unknown device';
+  static const String baseUrl = 'https://mobile-test-2d7e555a4f85.herokuapp.com/api/v1';
+}
