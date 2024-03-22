f5f3214c90727b1bcf5cc815a0f08e0886aacbae
kenresoft <amadikenneth8@gmail.com>
2024-03-22 20:35:39 +0100

    Update routes.dart

---
+++ lib/utils/routes.dart
@@ -0,0 +1,15 @@
+class RouteConstants {
+  RouteConstants._();
+
+  static const String splash = '/';
+  static const String onboarding = '/onboarding';
+  static const String signUp = '/signUp';
+  static const String signIn = '/signIn';
+  static const String otp = '/otp';
+  static const String identity = '/identity';
+  static const String pin = '/pin';
+  static const String confirmation = '/confirmation';
+  static const String passwordRecovery = '/passwordRecovery';
+  static const String verifyIdentity = '/verifyIdentity';
+  static const String resetPassword = '/resetPassword';
+}
